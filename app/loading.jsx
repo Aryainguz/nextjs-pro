aaf2afc8ac97aa02e2df4d0cd6e73f34adf1c678
Aryainguz <aryaninguz369@gmail.com>
2023-11-22 13:17:47 +0530

    added loading component

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex justify-center align-middle'>
+       <h1>loading...</h1> 
+        </div>
+  )
+}
+
+export default loading
--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
+  const val = "Hello, World!"
   return (
     <>
       <div className="flex justify-evenly m-6 text-blue-500">
         <Link href="/"> Home  </Link>
         <Link href="/about">  About </Link>
-
       </div>
+      <h1 className='text-center'>Value is: {val}</h1>
     </>
   )
 }
