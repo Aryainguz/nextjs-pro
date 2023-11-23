a926a5119f100034bcefd16fcca7272c4042e882
Aryainguz <aryaninguz369@gmail.com>
2023-11-23 09:53:11 +0530

    added users route

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,7 @@ export default function Home() {
       <div className="flex justify-evenly m-6 text-blue-500">
         <Link href="/"> Home  </Link>
         <Link href="/about">  About </Link>
+        <Link href="/users">  Users </Link>
       </div>
       <h1 className='text-center'>Value is: {val}</h1>
     </>
--- utils/features.js
+++ utils/features.js
@@ -1,5 +1,8 @@
 export const getAllUsers = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    if(!res.ok) {
+        throw new Error(res.statusText)
+    }       
     const users = await res.json()
     console.log(users)
     return users
