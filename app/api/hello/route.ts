eea13d7869e0ad69a030bc303a6538f322b4db96
Aryainguz <aryaninguz369@gmail.com>
2024-07-26 11:38:10 +0530

    feat: repo and api initialization

---
+++ app/api/hello/route.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET(req:NextApiRequest) {
+    return NextResponse.json({ message: "Hello World!" });
+}
