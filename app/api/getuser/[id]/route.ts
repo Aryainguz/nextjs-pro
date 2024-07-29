872243e700d5e891660ac06d8af0d8db9f3c346d
Aryainguz <aryaninguz369@gmail.com>
2024-07-29 11:00:33 +0530

    feat: added req body accesebility in userid api

--- app/api/getuser/[id]/route.ts
+++ app/api/getuser/[id]/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest,context:{params:any}) {
+export async function POST(req:NextRequest,context:{params:any}) {
+
+    const body = await req.json()
+
 
     const {searchParams} = new URL(req.url)
     
@@ -9,7 +12,8 @@ export async function GET(req:NextRequest,context:{params:any}) {
 
     return NextResponse.json({
         id:param,
-        q:searchParams.get("userID")
+        q:searchParams.get("userID"),
+        body:body
     })
     
 }
